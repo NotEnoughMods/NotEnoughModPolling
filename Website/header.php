fb6361df3685f5152caa29abca016ab6661b100c
Trent Monahan <sinz@d3s.co>
2013-10-20 01:37:52 +1100

    Started work on the web frontend
    preview can be found at http://nemp.mca.d3s.co/

---
+++ Website/header.php
@@ -0,0 +1,2 @@
+<?php
+echo("This is the header! <br/>");
---
+++ Website/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>NEMP</title>
+</head>
+<body>
+<?php
+include "./header.php";
+include "./body.php";
+include "./footer.php";
+?>
+</body>
+</html>
