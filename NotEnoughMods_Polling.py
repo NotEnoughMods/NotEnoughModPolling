56b5d5c5de54f9645884edffb1e01ffcadd59ef9
Pedro Cunha <pedroagracio@gmail.com>
2015-01-17 17:09:16 +0000

    Alert us of failed mods

--- NotEnoughMods_Polling.py
+++ NotEnoughMods_Polling.py
@@ -298,6 +298,9 @@ def NEMP_TimerEvent(self, channels):
                     self.NEM.mods[mod]["active"] = False
                     del self.NEM_troubledMods[mod]
 
+                    # TODO: Not hardcode the channel
+                    self.sendMessage('#test', 'Mod {0} failed.'.format(mod))
+
                     nemp_logger.debug("Mod {0} has failed to be polled at least 5 times, it has been disabled.".format(mod))
                     self.NEM.buildHTML()
 
