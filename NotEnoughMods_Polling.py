NotEnoughMods/NotEnoughModPolling

commit 862b94ac92efb06eddb42959627a0cf478c8f6de

Replace spaces with hyphens in the mod versions

NotEnoughMods_Polling.py
@@ -2,6 +2,7 @@ import traceback
 import time
 import logging
 import shlex
+import re
 
 
 from commands.NEMP import NEMP_Class
@@ -280,6 +281,10 @@ def NEMP_TimerEvent(self, channels):
                     else:
                         real_name = mod
 
+                    # Make sure there are no spaces in the versions
+                    self.NEM.mods[mod]['dev'] = re.sub(r'\s+', '-', self.NEM.mods[mod]['dev'])
+                    self.NEM.mods[mod]['version'] = re.sub(r'\s+', '-', self.NEM.mods[mod]['version'])
+
                     if self.NEM.mods[mod]["dev"] != "NOT_USED" and flags[0]:
                         nemp_logger.debug("Updating DevMod {0}, Flags: {1}".format(mod, flags))
                         self.sendMessage(channel, "!ldev {0} {1} {2}".format(version, real_name, unicode(self.NEM.mods[mod]["dev"])))
@@ -465,8 +470,12 @@ def test_parser(self, name, params, channel, userdata, rank):
                 else:
                     self.sendMessage(channel, "Did not receive MC version from parser.")
                 if "version" in result:
+                    # Make sure there are no spaces
+                    result['version'] = re.sub(r'\s+', '-', result['version'])
                     self.sendMessage(channel, "!lmod {0} {1} {2}".format(version, real_name, unicode(result["version"])))
                 if "dev" in result:
+                    # Make sure there are no spaces
+                    result['dev'] = re.sub(r'\s+', '-', result['dev'])
                     self.sendMessage(channel, "!ldev {0} {1} {2}".format(version, real_name, unicode(result["dev"])))
                 if "change" in result:
                     self.sendMessage(channel, " * " + result["change"])

commit 4011d7dea8b75543040e91e732061e8b1805857c

Fixing some stuff that was broken by 862b94a

NotEnoughMods_Polling.py
@@ -241,6 +241,11 @@ def PollingThread(self, pipe):
             else:
                 time.sleep(30)
 
+# Returns the version string with some replacements, like:
+# - whitespace (space/tab/etc) replaced by hyphen
+def clean_version(version):
+    return re.sub(r'\s+', '-', version)
+
 def NEMP_TimerEvent(self, channels):
     yes = self.threading.poll("NEMP")
 
@@ -281,17 +286,13 @@ def NEMP_TimerEvent(self, channels):
                     else:
                         real_name = mod
 
-                    # Make sure there are no spaces in the versions
-                    self.NEM.mods[mod]['dev'] = re.sub(r'\s+', '-', self.NEM.mods[mod]['dev'])
-                    self.NEM.mods[mod]['version'] = re.sub(r'\s+', '-', self.NEM.mods[mod]['version'])
-
                     if self.NEM.mods[mod]["dev"] != "NOT_USED" and flags[0]:
                         nemp_logger.debug("Updating DevMod {0}, Flags: {1}".format(mod, flags))
-                        self.sendMessage(channel, "!ldev {0} {1} {2}".format(version, real_name, unicode(self.NEM.mods[mod]["dev"])))
+                        self.sendMessage(channel, "!ldev {0} {1} {2}".format(version, real_name, unicode(clean_version(self.NEM.mods[mod]["dev"]))))
 
                     if self.NEM.mods[mod]["version"] != "NOT_USED" and flags[1]:
                         nemp_logger.debug("Updating Mod {0}, Flags: {1}".format(mod, flags))
-                        self.sendMessage(channel, "!lmod {0} {1} {2}".format(version, real_name, unicode(self.NEM.mods[mod]["version"])))
+                        self.sendMessage(channel, "!lmod {0} {1} {2}".format(version, real_name, unicode(clean_version(self.NEM.mods[mod]["version"]))))
 
                     if self.NEM.mods[mod]["change"] != "NOT_USED" and "changelog" not in self.NEM.mods[mod]:
                         nemp_logger.debug("Sending text for Mod {0}".format(mod))
@@ -470,13 +471,9 @@ def test_parser(self, name, params, channel, userdata, rank):
                 else:
                     self.sendMessage(channel, "Did not receive MC version from parser.")
                 if "version" in result:
-                    # Make sure there are no spaces
-                    result['version'] = re.sub(r'\s+', '-', result['version'])
-                    self.sendMessage(channel, "!lmod {0} {1} {2}".format(version, real_name, unicode(result["version"])))
+                    self.sendMessage(channel, "!lmod {0} {1} {2}".format(version, real_name, unicode(clean_version(result["version"]))))
                 if "dev" in result:
-                    # Make sure there are no spaces
-                    result['dev'] = re.sub(r'\s+', '-', result['dev'])
-                    self.sendMessage(channel, "!ldev {0} {1} {2}".format(version, real_name, unicode(result["dev"])))
+                    self.sendMessage(channel, "!ldev {0} {1} {2}".format(version, real_name, unicode(clean_version(result["dev"]))))
                 if "change" in result:
                     self.sendMessage(channel, " * " + result["change"])
 