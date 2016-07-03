c248bfc0463e74e62a95b784f42aa79f954ea0af
Pedro Cunha <pedroagracio@gmail.com>
2016-07-03 22:52:04 +0100

    Validate duplicate keys in mods.json

--- tests/test_mods_json.py
+++ tests/test_mods_json.py
@@ -3,12 +3,23 @@ import simplejson
 
 from NEMP import NEMP_Class
 
+# From http://stackoverflow.com/a/14902564/134960
+def dict_raise_on_duplicates(ordered_pairs):
+    """Reject duplicate keys."""
+    d = {}
+    for k, v in ordered_pairs:
+        if k in d:
+           raise ValueError("duplicate key: %r" % (k,))
+        else:
+           d[k] = v
+    return d
+
 class TestModsJson(unittest.TestCase):
     def setUp(self):
         self.NEM = NEMP_Class.NotEnoughClasses
 
         with open('NEMP/mods.json', 'r') as f:
-            self.mods = simplejson.load(f)
+            self.mods = simplejson.load(f, object_pairs_hook=dict_raise_on_duplicates)
 
     def test_parsers_exist(self):
         for mod, mod_info in self.mods.iteritems():
